f73676aaa5cf1944e4d8a55e1e0f6c55fa166c55
caternuson <methuzla@speakeasy.net>
2018-08-14 10:17:34 -0700

    added working code

---
+++ README.rst
@@ -0,0 +1,112 @@
+Introduction
+============
+
+.. image:: https://readthedocs.org/projects/adafruit-circuitpython-tca9548a/badge/?version=latest
+    :target: https://circuitpython.readthedocs.io/projects/tca9548a/en/latest/
+    :alt: Documentation Status
+
+.. image:: https://img.shields.io/discord/327254708534116352.svg
+    :target: https://discord.gg/nBQh6qu
+    :alt: Discord
+
+.. image:: https://travis-ci.org/adafruit/Adafruit_CircuitPython_TCA9548A.svg?branch=master
+    :target: https://travis-ci.org/adafruit/Adafruit_CircuitPython_TCA9548A
+    :alt: Build Status
+
+CircuitPython driver for the TCA9548A I2C Multiplexer.
+
+Dependencies
+=============
+This driver depends on:
+
+* `Adafruit CircuitPython <https://github.com/adafruit/circuitpython>`_
+* `Bus Device <https://github.com/adafruit/Adafruit_CircuitPython_BusDevice>`_
+
+Please ensure all dependencies are available on the CircuitPython filesystem.
+This is easily achieved by downloading
+`the Adafruit library and driver bundle <https://github.com/adafruit/Adafruit_CircuitPython_Bundle>`_.
+
+Usage Example
+=============
+
+```python
+# This example shows using two TSL2491 light sensors attached to TCA9548A channels 0 and 1.
+# Use with other I2C sensors would be similar.
+import time
+import board
+import busio
+import adafruit_tsl2591
+import adafruit_tca9548a
+
+# Create I2C bus as normal
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the TCA9548A object and give it the I2C bus
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+# For each sensor, create it using the TCA9548A channel instead of the I2C object
+tsl1 = adafruit_tsl2591.TSL2591(tca[0])
+tsl2 = adafruit_tsl2591.TSL2591(tca[1])
+
+# Loop and profit!
+while True:
+    print(tsl1.lux, tsl2.lux)
+    time.sleep(0.1)
+```
+
+Contributing
+============
+
+Contributions are welcome! Please read our `Code of Conduct
+<https://github.com/adafruit/Adafruit_CircuitPython_TCA9548A/blob/master/CODE_OF_CONDUCT.md>`_
+before contributing to help this project stay welcoming.
+
+Building locally
+================
+
+Zip release files
+-----------------
+
+To build this library locally you'll need to install the
+`circuitpython-build-tools <https://github.com/adafruit/circuitpython-build-tools>`_ package.
+
+.. code-block:: shell
+
+    python3 -m venv .env
+    source .env/bin/activate
+    pip install circuitpython-build-tools
+
+Once installed, make sure you are in the virtual environment:
+
+.. code-block:: shell
+
+    source .env/bin/activate
+
+Then run the build:
+
+.. code-block:: shell
+
+    circuitpython-build-bundles --filename_prefix adafruit-circuitpython-tca9548a --library_location .
+
+Sphinx documentation
+-----------------------
+
+Sphinx is used to build the documentation based on rST files and comments in the code. First,
+install dependencies (feel free to reuse the virtual environment from above):
+
+.. code-block:: shell
+
+    python3 -m venv .env
+    source .env/bin/activate
+    pip install Sphinx sphinx-rtd-theme
+
+Now, once you have the virtual environment activated:
+
+.. code-block:: shell
+
+    cd docs
+    sphinx-build -E -W -b html . _build/html
+
+This will output the documentation to ``docs/_build/html``. Open the index.html in your browser to
+view them. It will also (due to -W) error out on any warning like Travis will. This is a good way to
+locally verify it will pass.
---
+++ adafruit_tca9548a.py
@@ -0,0 +1,96 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Carter Nelson for Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""
+`Adafruit_TCA9548A`
+====================================================
+
+CircuitPython driver for the TCA9548A I2C Multiplexer.
+
+* Author(s): Carter Nelson
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+* TCA9548A I2C Multiplexer: https://www.adafruit.com/product/2717
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+"""
+
+# imports
+from micropython import const
+
+_DEFAULT_ADDRESS = const(0x70)
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_TCA9548A.git"
+
+class TCA9548A_Channel():
+    """Helper class to represent an output channel on the TCA9548A and take care
+       of the necessary I2C commands for channel switching. This class needs to
+       behave like an I2CDevice."""
+
+    def __init__(self, tca, channel):
+        self.tca = tca
+        self.channel = channel
+        self.channel_switch = bytearray([1 << channel])
+
+    def try_lock(self):
+        while not self.tca.i2c.try_lock():
+            pass
+        self.tca.i2c.writeto(self.tca.address, self.channel_switch)
+        return True
+
+    def unlock(self):
+        return self.tca.i2c.unlock()
+
+    def readfrom_into(self, address, buffer, **kwargs):
+        if address == self.tca.address:
+            raise ValueError("Device address must be different than TCA9548A address.")
+        return self.tca.i2c.readfrom_into(address, buffer, **kwargs)
+
+    def writeto(self, address, buffer, **kwargs):
+        if address == self.tca.address:
+            raise ValueError("Device address must be different than TCA9548A address.")
+        return self.tca.i2c.writeto(address, buffer, **kwargs)
+
+
+class TCA9548A():
+    """Class which provides interface to TCA9548A I2C multiplexer."""
+
+    def __init__(self, i2c, address=_DEFAULT_ADDRESS):
+        self.i2c = i2c 
+        self.address = address
+        self.used_channels = []
+
+    def __getitem__(self, key):
+        if not 0 <= key <= 7:
+            raise ValueError("Channel must be an integer in the range: 0-7")
+        if key in self.used_channels:
+            raise ValueError("Channel already in use.")
+        self.used_channels.append(key)
+        return TCA9548A_Channel(self, key)
